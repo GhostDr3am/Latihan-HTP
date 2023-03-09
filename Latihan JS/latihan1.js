8b93b12eeb23048d3e3c7f0483a8203c082cfebe
dedi011002 <dedisupriadi011002@gmail.com>
2023-03-09 10:09:49 +0800

    Menambahkan Folder Latihan JS

---
+++ Latihan JS/latihan1.js
@@ -0,0 +1,9 @@
+let nama = 'Dedi Supriadi';
+let umur = 20;
+let pekerjaan = 'Mahasiswa';
+
+document.write('<h3 align="center"> Belajar JavaScript </h3>');
+document.write('<hr>');
+document.write('Nama : ' + nama + '<br>');
+document.write('Umur : ' + umur + ' Tahun' + '<br>');
+document.write('Pekerjaan : ' + pekerjaan + '<br>');
