f36a6db44f0295811d07d5a7d2e6e3934689cf65
Lyncx4rc13 <dedisupriadi011002@gmail.com>
2023-03-09 12:02:41 +0800

    Belajar Operator JS

---
+++ Latihan JS/latihan2.js
@@ -0,0 +1,6 @@
+let jari2 = 20;
+const phi = 3.14;
+let luas = phi * jari2 * jari2;
+keliling = 2 * phi * jari2;
+
+document.write("Diketahui jari-jari sebuah lingkaran = " + jari2 + " cm" + "<br>" + "Maka luas lingkaran tersebut adalah = " + luas + " cm" + "<sup>2</sup>" + "<br>" + "Maka keliling lingkaran tersebut adalah = " + keliling + " cm" + "<br>");
---
+++ Latihan JS/latihan3.js
@@ -0,0 +1,16 @@
+// Operator Perbandingan {==}
+let a = true;
+let b = true;
+
+console.log(a == b);
+
+let a1 = 7
+let b1 = "7"
+
+console.log(a1 == b1);
+
+// Operator pembanding tidak sama dengan (!=)
+let a2 = 23
+let b2 = 45
+
+console.log(a2 != b2);
