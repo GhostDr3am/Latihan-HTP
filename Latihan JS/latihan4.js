7aac1493d29fcdc5c1e74540d5f7084d3f5d9933
Lyncx4rc13 <dedisupriadi011002@gmail.com>
2023-03-10 10:44:36 +0800

    menambahkan latihan 4

---
+++ Latihan JS/latihan4.js
@@ -0,0 +1,10 @@
+let pelanggan = 'Dedi Supriadi'
+let totalBelanja = 50000
+let keterangan = ''
+
+if (totalBelanja > 100000) {
+  keterangan = 'Anda mendapatkan hadiah';
+} else {
+  keterangan = 'Terima kasih sudah berbelanja';
+}
+console.log(keterangan);
