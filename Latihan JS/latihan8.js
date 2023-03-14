06b08a189508144901aaab0577fc9d50bfd1e366
DxrkLyncx <dedisupriadi011002@gmail.com>
2023-03-14 10:31:09 +0800

    Menambahkan file latihan 8

---
+++ Latihan JS/latihan8.js
@@ -0,0 +1,25 @@
+// Perulangan For
+let angka;
+for (let angka = 1; angka <= 50; angka++) {
+  console.log('Bilangan ' + angka);
+}
+
+for (i = 50; i >= 1; i--) {
+  console.log('Bilangan ' + i);
+}
+
+// Mencetak nilai genap dan ganjil
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 == 0) {
+    console.log('Bilangan Genap ' + i);
+  } else {
+    console.log('Bilangan Ganjil ' + i);
+  }
+}
+
+// Perulangan while
+
+while (i <= 50) {
+  console.log('Bilangan ' + i);
+  i++;
+}
