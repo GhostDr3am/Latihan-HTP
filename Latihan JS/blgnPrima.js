7c872b45759441975615f961fa2c83b66328f01e
DxrkLyncx <dedisupriadi011002@gmail.com>
2023-03-14 10:38:02 +0800

    Menambahkan data bilangan prima

--- Latihan JS/blgnPrima.js
+++ Latihan JS/blgnPrima.js
@@ -1,9 +1,24 @@
-for (let i = 2; i <= 10; i++) {
+for (let i = 2; i <= 30; i++) {
   for (let j = 2; j < i; j++) {
     if (i == j + 1) {
       console.log(i + ' adalah bilangan prima');
     } else if (i % j == 0) {
       break;
     }
+  }
+}
+
+// Bilangan prima versi 2
+
+
+for (i = 1; i <= 50; i++) {
+  let prin = 0;
+  for (j = 1; j <= i; j++) {
+    if (i % j == 0) {
+      prin++;
+    }
+  }
+  if (prin == 2) {
+    console.log('bilangan prima ' + i);
   }
 }
